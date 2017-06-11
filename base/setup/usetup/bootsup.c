a311881caebe26ff387441a147ed362359327e4f
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2017-06-11 23:58:43 +0000

    [USETUP] Introduce SetupDeleteFile() and SetupMoveFile() (in addition to the already-existing SetupCopyFile()) in order to implement moving / renaming existing files.
    Will be used soon to make backups of system files, like the registry hive files just freshly created.
    - Make the SetupCopyFile() function closer to its win32 counterpart.
    - Adjust the code that calls SetupCopyFile().
    
    svn path=/branches/setup_improvements/; revision=75008
    svn path=/branches/setup_improvements/; revision=75009

--- base/setup/usetup/bootsup.c
+++ base/setup/usetup/bootsup.c
@@ -1757,7 +1757,7 @@ InstallFatBootcodeToPartition(
     CombinePaths(DstPath, ARRAYSIZE(DstPath), 2, SystemRootPath->Buffer, L"freeldr.sys");
 
     DPRINT("Copy: %S ==> %S\n", SrcPath, DstPath);
-    Status = SetupCopyFile(SrcPath, DstPath);
+    Status = SetupCopyFile(SrcPath, DstPath, FALSE);
     if (!NT_SUCCESS(Status))
     {
         DPRINT1("SetupCopyFile() failed (Status %lx)\n", Status);
@@ -2068,7 +2068,7 @@ InstallExt2BootcodeToPartition(
     CombinePaths(DstPath, ARRAYSIZE(DstPath), 2, SystemRootPath->Buffer, L"freeldr.sys");
 
     DPRINT("Copy: %S ==> %S\n", SrcPath, DstPath);
-    Status = SetupCopyFile(SrcPath, DstPath);
+    Status = SetupCopyFile(SrcPath, DstPath, FALSE);
     if (!NT_SUCCESS(Status))
     {
         DPRINT1("SetupCopyFile() failed (Status %lx)\n", Status);
@@ -2233,7 +2233,7 @@ InstallFatBootcodeToFloppy(
     CombinePaths(DstPath, ARRAYSIZE(DstPath), 2, FloppyDevice.Buffer, L"freeldr.sys");
 
     DPRINT("Copy: %S ==> %S\n", SrcPath, DstPath);
-    Status = SetupCopyFile(SrcPath, DstPath);
+    Status = SetupCopyFile(SrcPath, DstPath, FALSE);
     if (!NT_SUCCESS(Status))
     {
         DPRINT1("SetupCopyFile() failed (Status %lx)\n", Status);
