bf907ac3315885dd70f29981317bc062d745ac6a
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2017-10-05 22:17:52 +0200

    [CMD] Small whitespace fix + use conutils functions in ErrorMessage(). CORE-13860

--- base/shell/cmd/error.c
+++ base/shell/cmd/error.c
@@ -28,10 +28,10 @@ ErrorMessage(
     IN LPTSTR szFormat OPTIONAL,
     ...)
 {
+    INT Len;
     va_list arg_ptr;
-    LPTSTR szError;
-    TCHAR szMsg[RC_STRING_MAX_SIZE];
     TCHAR szMessage[1024];
+    TCHAR szMsg[RC_STRING_MAX_SIZE];
 
     if (dwErrorCode == ERROR_SUCCESS)
         return;
@@ -44,13 +44,12 @@ ErrorMessage(
         va_end(arg_ptr);
     }
 
-    if (FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER,
-                      NULL, dwErrorCode, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-                      (LPTSTR)&szError, 0, NULL))
+    // ConErrFormatMessage(dwErrorCode);
+    Len = ConMsgPuts(StdErr, FORMAT_MESSAGE_FROM_SYSTEM,
+                     NULL, dwErrorCode, LANG_USER_DEFAULT);
+    if (Len > 0)
     {
-        ConErrPrintf(_T("%s%s%s"), szError, szMessage, (*szMessage ? _T("\n") : _T("")));
-        if (szError)
-            LocalFree(szError);
+        ConErrPrintf(_T("%s%s"), szMessage, (*szMessage ? _T("\n") : _T("")));
         return;
     }
 
@@ -69,35 +68,35 @@ VOID error_parameter_format(TCHAR ch)
 }
 
 
-VOID error_invalid_switch (TCHAR ch)
+VOID error_invalid_switch(TCHAR ch)
 {
     ConErrResPrintf(STRING_ERROR_INVALID_SWITCH, ch);
     nErrorLevel = 1;
 }
 
 
-VOID error_too_many_parameters (LPTSTR s)
+VOID error_too_many_parameters(LPTSTR s)
 {
     ConErrResPrintf(STRING_ERROR_TOO_MANY_PARAMETERS, s);
     nErrorLevel = 1;
 }
 
 
-VOID error_path_not_found (VOID)
+VOID error_path_not_found(VOID)
 {
     ConErrResPuts(STRING_ERROR_PATH_NOT_FOUND);
     nErrorLevel = 1;
 }
 
 
-VOID error_file_not_found (VOID)
+VOID error_file_not_found(VOID)
 {
     ConErrResPuts(STRING_ERROR_FILE_NOT_FOUND);
     nErrorLevel = 1;
 }
 
 
-VOID error_sfile_not_found (LPTSTR f)
+VOID error_sfile_not_found(LPTSTR f)
 {
     TCHAR szMsg[RC_STRING_MAX_SIZE];
 
@@ -107,49 +106,49 @@ VOID error_sfile_not_found (LPTSTR f)
 }
 
 
-VOID error_req_param_missing (VOID)
+VOID error_req_param_missing(VOID)
 {
     ConErrResPuts(STRING_ERROR_REQ_PARAM_MISSING);
     nErrorLevel = 1;
 }
 
 
-VOID error_invalid_drive (VOID)
+VOID error_invalid_drive(VOID)
 {
     ConErrResPuts(STRING_ERROR_INVALID_DRIVE);
     nErrorLevel = 1;
 }
 
 
-VOID error_bad_command (LPTSTR s)
+VOID error_bad_command(LPTSTR s)
 {
     ConErrResPrintf(STRING_ERROR_BADCOMMAND, s);
     nErrorLevel = 9009;
 }
 
 
-VOID error_no_pipe (VOID)
+VOID error_no_pipe(VOID)
 {
     ConErrResPuts(STRING_ERROR_CANNOTPIPE);
     nErrorLevel = 1;
 }
 
 
-VOID error_out_of_memory (VOID)
+VOID error_out_of_memory(VOID)
 {
     ConErrResPuts(STRING_ERROR_OUT_OF_MEMORY);
     nErrorLevel = 1;
 }
 
 
-VOID error_invalid_parameter_format (LPTSTR s)
+VOID error_invalid_parameter_format(LPTSTR s)
 {
     ConErrResPrintf(STRING_ERROR_INVALID_PARAM_FORMAT, s);
     nErrorLevel = 1;
 }
 
 
-VOID error_syntax (LPTSTR s)
+VOID error_syntax(LPTSTR s)
 {
     TCHAR szMsg[RC_STRING_MAX_SIZE];
 
@@ -164,7 +163,7 @@ VOID error_syntax (LPTSTR s)
 }
 
 
-VOID msg_pause (VOID)
+VOID msg_pause(VOID)
 {
     ConOutResPuts(STRING_ERROR_D_PAUSEMSG);
 }
