9756b9a278b38ed23e103622d9e8e3da3738cfbd
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2019-09-25 02:01:47 +0200

    StartROM - Add a PE loader (in 32/64 bits environment) based from the code of OBJ2BIN, FreeLoader and Alex' BootMgr library.
    The PE loader finds any valid PE appended to the StartROM module -- it
    must be aligned on a file sector boundary 0x200, and at most should be
    present at twice this distance from the end of StartROM.
    Then it loads it at its preferred base, unfolding its sections as
    expected, retrieves its entry-point (with the same prototype as
    NtProcessStartup() ) and start it.
    
    Informative interactive prompts are displayed in the process.
    
    Add also PcVideoGetTextCursorPosition() to get the new cursor
    coordinates after going back from the started demo PE.

--- boot/freeldr/freeldr/main.c
+++ boot/freeldr/freeldr/main.c
@@ -31,13 +31,29 @@ objcopy c:\Temp\Essai\main.exe -O binary c:\Temp\Essai\main_objcopy.bin
 #include <arch/pc/pcbios.h>
 // #include <arch/pc/machpc.h>
 #include <arch/pc/x86common.h>
-#include <arch/pc/pxe.h>
-// #include <arch/i386/drivemap.h>
+// #include <arch/pc/pxe.h>
 
 #if defined(_M_IX86)
 #include <arch/i386/i386.h>
 #include <internal/i386/intrin_i.h>
 #endif
+
+
+///////////////////////
+
+extern ULONG _bss_end__;
+
+
+#define ROUND_DOWN(n, align) \
+    (((ULONG_PTR)(n)) & ~((align) - 1l))
+
+#define ROUND_UP(n, align) \
+    ROUND_DOWN(((ULONG_PTR)(n)) + (align) - 1, (align))
+
+#define RVA(m, b) ((PVOID)((ULONG_PTR)(b) + (ULONG_PTR)(m)))
+
+#define IMAGE_FILE_RELOCS_STRIPPED      0x0001
+#define IMAGE_FILE_EXECUTABLE_IMAGE     0x0002
 
 
 ///////////////////////
@@ -183,6 +199,33 @@ PcVideoPutChar(int Ch, UCHAR Attr, unsigned X, unsigned Y)
 
   BufPtr = (USHORT *) (ULONG_PTR)(VIDEOTEXT_MEM_ADDRESS + Y * BytesPerScanLine + X * 2);
   *BufPtr = ((USHORT) Attr << 8) | (Ch & 0xff);
+}
+
+VOID
+PcVideoGetTextCursorPosition(ULONG* X, ULONG* Y)
+{
+    REGS    Regs;
+
+    // Int 10h AH=03h
+    // VIDEO - GET CURSOR POSITION AND SIZE
+    //
+    // AH = 03h
+    // BH = page number
+    // 0-3 in modes 2&3
+    // 0-7 in modes 0&1
+    // 0 in graphics modes
+    // Return:
+    // AX = 0000h (Phoenix BIOS)
+    // CH = start scan line
+    // CL = end scan line
+    // DH = row (00h is top)
+    // DL = column (00h is left)
+    Regs.b.ah = 0x03;
+    Regs.b.bh = 0x00;
+    Int386(0x10, &Regs, &Regs);
+
+    *X = Regs.b.dl;
+    *Y = Regs.b.dh;
 }
 
 VOID
@@ -513,12 +556,292 @@ int __cdecl iswctype(wint_t wc, wctype_t wctypeFlags)
 }
 
 
+VOID
+NTAPI
+RtlAssert(IN PVOID FailedAssertion,
+          IN PVOID FileName,
+          IN ULONG LineNumber,
+          IN PCHAR Message OPTIONAL)
+{
+    if (Message)
+    {
+        PrintText("Assertion \'%s\' failed at %s line %lu: %s\n",
+                 (PCHAR)FailedAssertion,
+                 (PCHAR)FileName,
+                 LineNumber,
+                 Message);
+    }
+    else
+    {
+        PrintText("Assertion \'%s\' failed at %s line %lu\n",
+                 (PCHAR)FailedAssertion,
+                 (PCHAR)FileName,
+                 LineNumber);
+    }
+
+    // DbgBreakPoint();
+    for (;;);
+}
+
+
+static BOOLEAN
+ValidatePEHeader( // ParsePEImage
+    IN PVOID ImageBuffer,
+    // IN ULONG nImageSize,
+    OUT PIMAGE_NT_HEADERS* pNtHeaders,
+    OUT PIMAGE_FILE_HEADER* pFileHeader)
+{
+    PIMAGE_DOS_HEADER DosHeader = (PIMAGE_DOS_HEADER)ImageBuffer;
+    /*
+     * NOTE: The correct structure that we support (IMAGE_OPTIONAL_HEADER32 or
+     * IMAGE_OPTIONAL_HEADER64) is automatically selected at compilation time.
+     */
+    PIMAGE_OPTIONAL_HEADER OptionalHeader;
+    ULONG NtHeaderOffset;
+    // ULONG TotalHeadersSize = 0;
+
+    /* Ensure it's a PE image */
+    if (!(/* nImageSize >= sizeof(IMAGE_DOS_HEADER) && */ DosHeader->e_magic == IMAGE_DOS_SIGNATURE))
+    {
+        /* Fail */
+        PrintText("0x%p - Not a valid PE image!\n", ImageBuffer);
+        return FALSE;
+    }
+
+    /* Get the offset to the NT headers */
+    NtHeaderOffset = DosHeader->e_lfanew;
+
+#if 0
+    /* Make sure the file header fits into the size */
+    TotalHeadersSize += NtHeaderOffset +
+                        FIELD_OFFSET(IMAGE_NT_HEADERS, FileHeader) + (sizeof(((IMAGE_NT_HEADERS *)0)->FileHeader));
+    if (TotalHeadersSize >= nImageSize)
+    {
+        /* Fail */
+        PrintText("0x%p - NT headers beyond image size!\n", ImageBuffer);
+        return FALSE;
+    }
+#endif
+
+    /* Now get a pointer to the NT Headers */
+    *pNtHeaders = (PIMAGE_NT_HEADERS)RVA(ImageBuffer, NtHeaderOffset);
+
+    /* Verify the PE Signature */
+    if ((*pNtHeaders)->Signature != IMAGE_NT_SIGNATURE)
+    {
+        /* Fail */
+        PrintText("0x%p - Invalid image NT signature!\n", ImageBuffer);
+        return FALSE;
+    }
+
+    /* Ensure this is an executable image */
+    if (((*pNtHeaders)->FileHeader.Characteristics & IMAGE_FILE_EXECUTABLE_IMAGE) == 0)
+    {
+        /* Fail */
+        PrintText("0x%p - Invalid executable image!\n", ImageBuffer);
+        return FALSE;
+    }
+
+    /* Get the COFF header */
+    *pFileHeader = &(*pNtHeaders)->FileHeader;
+
+    /* Check for the presence of the optional header */
+    if ((*pFileHeader)->SizeOfOptionalHeader == 0)
+    {
+        /* Fail */
+        PrintText("0x%p - Unsupported PE image (no optional header)!\n", ImageBuffer);
+        return FALSE;
+    }
+
+#if 0
+    /* Make sure the optional file header fits into the size */
+    TotalHeadersSize += (*pFileHeader)->SizeOfOptionalHeader;
+    if (TotalHeadersSize >= nImageSize)
+    {
+        /* Fail */
+        PrintText("0x%p - NT optional header beyond image size!\n", ImageBuffer);
+        return FALSE;
+    }
+#endif
+
+    /*
+     * Retrieve the optional header and be sure that its size corresponds
+     * to its signature. Note that we only support either x86 or x64 images.
+     */
+    OptionalHeader = (PVOID)(*pFileHeader + 1);
+    if (!((*pFileHeader)->SizeOfOptionalHeader == sizeof(IMAGE_OPTIONAL_HEADER) &&
+          OptionalHeader->Magic == IMAGE_NT_OPTIONAL_HDR_MAGIC))
+    {
+        /* Fail */
+        PrintText("0x%p - Invalid or unrecognized NT optional header!\n", ImageBuffer);
+        return FALSE;
+    }
+
+    return TRUE;
+}
+
+static PVOID
+LoadPEImage(
+    IN PVOID ImageBuffer,
+    IN PIMAGE_NT_HEADERS NtHeaders,
+    IN PIMAGE_FILE_HEADER FileHeader)
+{
+    PVOID ImageBase;
+    /*
+     * NOTE: The correct structure that we support (IMAGE_OPTIONAL_HEADER32 or
+     * IMAGE_OPTIONAL_HEADER64) is automatically selected at compilation time.
+     */
+    PIMAGE_OPTIONAL_HEADER OptionalHeader;
+    PIMAGE_SECTION_HEADER pSection;
+    ULONG SizeOfImage, SizeOfHeaders, SectionAlignment;
+    ULONG SectionSize, RawSize;
+    ULONG i;
+
+    /* Be sure the user didn't pass inconsistent information */
+    ASSERT(&NtHeaders->FileHeader == FileHeader);
+
+    /* Retrieve the optional header, its validity has been checked in ParsePEImage() */
+    OptionalHeader = (PVOID)(FileHeader + 1);
+    ASSERT(OptionalHeader == &NtHeaders->OptionalHeader);
+
+    if (!(FileHeader->SizeOfOptionalHeader == sizeof(IMAGE_OPTIONAL_HEADER) &&
+          OptionalHeader->Magic == IMAGE_NT_OPTIONAL_HDR_MAGIC))
+    {
+        /* Fail */
+        PrintText("0x%p - Invalid or unrecognized NT optional header!\n", ImageBuffer);
+        return NULL;
+    }
+
+    /* Check for supported architecture - We only support either x86 or x64 images */
+#if defined(_M_IX86)
+    if (FileHeader->Machine != IMAGE_FILE_MACHINE_I386)
+#elif defined(_M_AMD64) || defined(_M_X64)
+    if (FileHeader->Machine != IMAGE_FILE_MACHINE_AMD64)
+#else
+#error Invalid architecture. StartROM is designed only for x86 or x64 architectures!
+#endif
+    {
+        PrintText("0x%p - Unsupported machine type 0x%04x!\n", ImageBuffer, FileHeader->Machine);
+        return NULL;
+    }
+
+    /* We can only load native boot images */
+    if (OptionalHeader->Subsystem != IMAGE_SUBSYSTEM_NATIVE)
+    {
+        PrintText("0x%p - Unsupported subsystem type 0x%04x!\n", ImageBuffer, OptionalHeader->Subsystem);
+        return NULL;
+    }
+
+    /* Find the actual size of the image in memory, and the size of the headers in the file */
+    SizeOfImage = OptionalHeader->SizeOfImage;
+    SizeOfHeaders = OptionalHeader->SizeOfHeaders;
+    SectionAlignment = OptionalHeader->SectionAlignment;
+
+    // TODO: We could check this in the ValidatePEImage() function above...
+    if (SizeOfImage < SizeOfHeaders)
+    {
+        /* Bail out if they're bigger than the image! */
+        // STATUS_INVALID_IMAGE_FORMAT;
+        PrintText("0x%p - This PE image has an invalid size of headers (0x%lu, expected less than 0x%lu)!\n", ImageBuffer, SizeOfHeaders, SizeOfImage);
+        return NULL;
+    }
+
+    /* If there are actually no sections in this image, fail */
+    if (FileHeader->NumberOfSections == 0)
+    {
+        PrintText("0x%p - This PE image does not have any sections!\n", ImageBuffer);
+        return NULL;
+    }
+
+    /* Retrieve the image preferred load address */
+    ImageBase = (PVOID)OptionalHeader->ImageBase;
+    if (!ImageBase)
+    {
+        PrintText("0x%p - This PE image has an invalid image base (0x%p)!\n", ImageBuffer, ImageBase);
+        return NULL;
+    }
+    //
+    // TODO: We should check whether the memory area between
+    // ImageBase and (ULONG_PTR)ImageBase + SizeOfImage is free.
+    // If not, complain and bail out.
+    //
+
+    /* Load the PE image headers to destination */
+    RtlMoveMemory(ImageBase, ImageBuffer, NtHeaders->OptionalHeader.SizeOfHeaders);
+
+    /* Iterate through the sections and load them at their respective address */
+    pSection = RVA(FileHeader + 1, FileHeader->SizeOfOptionalHeader);
+    for (i = 0; i < FileHeader->NumberOfSections; ++i, ++pSection)
+    {
+        /* Make sure that the section fits within the image */
+        if ((pSection->VirtualAddress > SizeOfImage) ||
+            (RVA(ImageBase, pSection->VirtualAddress) < ImageBase))
+        {
+            PrintText("0x%p - Section '%s' outside of the image.\n", ImageBuffer, (char*)pSection->Name);
+            return NULL;
+        }
+
+        /* Get the section virtual size and the raw size */
+        SectionSize = pSection->Misc.VirtualSize;
+        RawSize = pSection->SizeOfRawData;
+
+        /* Handle a case when VirtualSize equals 0 */
+        if (SectionSize == 0)
+            SectionSize = RawSize;
+
+        /* If PointerToRawData is 0, then force its size to be also 0 */
+        if (pSection->PointerToRawData == 0)
+            RawSize = 0;
+        /* Truncate the loaded size to the VirtualSize extents */
+        else if (RawSize > SectionSize)
+            RawSize = SectionSize;
+
+        /* Actually read the section (if its size is not 0) */
+        if (RawSize != 0)
+        {
+            /* Read this section from the file, size = SizeOfRawData */
+            RtlMoveMemory(RVA(ImageBase, pSection->VirtualAddress),
+                          RVA(ImageBuffer, pSection->PointerToRawData),
+                          RawSize);
+        }
+
+        /* Size of data is less than the virtual size - fill up the remainder with zeroes */
+        if (RawSize < SectionSize)
+        {
+            RtlZeroMemory(RVA(ImageBase, pSection->VirtualAddress + RawSize),
+                          SectionSize - RawSize);
+        }
+    }
+
+#if 0 // BOOTMGR.EXE / OSLOADER.EXE does not have relocations (yet...)
+    /* Relocate the image */
+    if (!(BOOLEAN)LdrRelocateImage(ImageBase,
+                                   // NtHeaders,
+                                   TRUE,
+                                   TRUE, /* In case of conflict still return success */
+                                   FALSE))
+    {
+        PrintText("0x%p - Failed to relocate image!\n", ImageBuffer);
+        return NULL;
+    }
+#endif
+
+    /* We succeeded, return the (physical address of the) entry point */
+    return RVA(ImageBase, OptionalHeader->AddressOfEntryPoint);
+}
+
+// NOTE: Same prototype as NtProcessStartup().
+typedef VOID (NTAPI *BOOTMGR_ENTRY_POINT) (PVOID ptr /*PLOADER_PARAMETER_BLOCK LoaderBlock*/);
 
 VOID _cdecl BootMain(PVOID ptr)
 {
     INT i;
     ULONG oldScreenPosX;
     ULONG oldScreenPosY;
+    ULONG_PTR ImageBase;
+    PIMAGE_NT_HEADERS NtHeaders;
+    PIMAGE_FILE_HEADER FileHeader;
+    BOOTMGR_ENTRY_POINT EntryPoint;
 
     PcVideoHideShowTextCursor(FALSE);
     PcVideoSetTextCursorPosition(0, 0);
@@ -526,7 +849,7 @@ VOID _cdecl BootMain(PVOID ptr)
     // PcConsPutChar('H');
     PcVideoClearScreen(0x50 | 0x0E); // Background purple, foreground yellow
     PrintTextColor(0x50 | 0x0E, "\nHello from StartROM!\n====================\n");
-    PrintTextColor(0x90 | 0x0F, "\n\nPress any key to quit..."); // Background-intensity bit 0x80 triggers blinking.
+    PrintTextColor(0x90 | 0x0F, "\n\nPress any key to load..."); // Background-intensity bit 0x80 triggers blinking.
     for (;;)
     {
         if (PcConsKbHit())
@@ -537,8 +860,65 @@ VOID _cdecl BootMain(PVOID ptr)
         }
         PcHwIdle();
     }
+    i386_ScreenPosX = 0;
+    PrintTextColor(0x50 | 0x0F, "                        "); // Erase the prompt
+    PrintText("\n\n");
 
-    PrintTextColor(0x10 | 0x0F, "\n\nWe quit!\n\n");
+
+    /*
+     * Search for a valid PE image (BOOTMGR.EXE / OSLOADER.EXE) that is
+     * appended after us, aligned on file sector boundary 0x200.
+     */
+    ImageBase = ROUND_UP((ULONG_PTR)&_bss_end__, 0x200);
+    if (!ValidatePEHeader((PVOID)ImageBase, &NtHeaders, &FileHeader))
+    {
+        /* Try again one sector after; if we fail, bail out */
+        ImageBase += 0x200;
+        if (!ValidatePEHeader((PVOID)ImageBase, &NtHeaders, &FileHeader))
+        {
+            PrintTextColor(0x10 | 0x0F, "\nCould not find a valid OSLOADER.EXE! Rebooting...\n");
+            goto Fail;
+        }
+    }
+
+    /* Load it at its preferred base address */
+    EntryPoint = (BOOTMGR_ENTRY_POINT)LoadPEImage((PVOID)ImageBase /*ImageBuffer*/,
+                                                  NtHeaders, FileHeader);
+    if (!EntryPoint)
+    {
+        PrintTextColor(0x10 | 0x0F, "\nCould not load OSLOADER.EXE! Rebooting...\n");
+        goto Fail;
+    }
+
+    /* TODO: Prepare some extra environment ??? (IDT, GDT, Stack) */
+
+    PcVideoSetTextCursorPosition(i386_ScreenPosX, i386_ScreenPosY);
+
+    /* Pass control to it */
+    //
+    // FIXME: We shall send instead a boot block record
+    // containing different type of information and a table
+    // to services exported by StartROM.
+    //
+    (*EntryPoint)(&Int386);
+
+    /* If we reached there, we somehow failed to start, just reboot */
+
+    PcVideoGetTextCursorPosition(&i386_ScreenPosX, &i386_ScreenPosY);
+
+Fail:
+    PrintTextColor(0x10 | 0x0F, "\n\nPress any key to restart...");
+    for (;;)
+    {
+        if (PcConsKbHit())
+        {
+            if (PcConsGetCh() == KEY_EXTENDED)
+                PcConsGetCh();
+            break;
+        }
+        PcHwIdle();
+    }
+    PrintText("\n\n");
     // Poor-man's wait.
     oldScreenPosX = i386_ScreenPosX;
     oldScreenPosY = i386_ScreenPosY;
@@ -549,40 +929,4 @@ VOID _cdecl BootMain(PVOID ptr)
         i386_ScreenPosY = oldScreenPosY;
         PcHwIdle();
     }
-
-    //
-    // TODO:
-    //
-    // 1. Determine the offset of the last part of our binary.
-    //
-    // 2. After it should be the PE code of OSLOADER.EXE / BOOTMGR.EXE .
-    //    We might as well try to loop for a little while in memory in order
-    //    to search for a valid MZ / PE header, but not too far otherwise
-    //    we need to fail and show an error message (+ prompting for restart).
-    //
-    // 3. If everything succeeded so far, we have to load the image in memory,
-    //    see e.g. obj2bin code for PE, then (OPTIONAL?) apply relocations
-    //    and jump to the entry point of the PE.
-    //
-    // 4. Any return from the entry point should trigger an immediate reboot.
-    //
-#if 0
-
-    /* Address the image with es segment */
-    mov ax, FREELDR_PE_BASE / 16
-    mov es, ax
-
-#define IMAGE_DOS_HEADER_e_lfanew 60
-#define IMAGE_FILE_HEADER_SIZE 20
-#define IMAGE_OPTIONAL_HEADER_AddressOfEntryPoint 16
-
-    /* Get address of optional header */
-    mov eax, dword ptr es:[IMAGE_DOS_HEADER_e_lfanew]
-    add eax, 4 + IMAGE_FILE_HEADER_SIZE
-
-    /* Get address of entry point */
-    mov eax, dword ptr es:[eax + IMAGE_OPTIONAL_HEADER_AddressOfEntryPoint]
-    add eax, FREELDR_PE_BASE
-
-#endif
 }
