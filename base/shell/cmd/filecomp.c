HBelusca/reactos

commit 3046ad12ecbe0f025b757018bcfddb6bd313fe51

Andrey Janzen (virus126): - Fix warning when using FEATURE_UNIX_FILENAME_COMPLETION. - Make unix-style completion work correctly. - Make %time% return milliseconds/10.
git-svn-id: svn://svn.reactos.org/reactos/trunk/reactos@25896 97493ccd-5924-5043-b1f5-66cb403b36ce

base/shell/cmd/filecomp.c
@@ -43,7 +43,7 @@ VOID CompleteFilename (LPTSTR str, UINT charcount)
 	LPCOMMAND cmds_ptr;
 
 	/* expand current file name */
-	count = charcount - 1;
+        count = charcount - 1;
 	if (count < 0)
 		count = 0;
 
@@ -89,6 +89,7 @@ VOID CompleteFilename (LPTSTR str, UINT charcount)
 	_tcscpy (path, directory);
 
 	while (curplace >= 0 && directory[curplace] != _T('\\') &&
+                   directory[curplace] != _T('/') &&
 		   directory[curplace] != _T(':'))
 	{
 		directory[curplace] = 0;
@@ -205,7 +206,7 @@ BOOL ShowCompletionMatches (LPTSTR str, INT charcount)
 	BOOL  found_dot = FALSE;
 	INT   curplace = 0;
 	INT   start;
-	UINT   count;
+	INT   count;
 	TCHAR path[MAX_PATH];
 	TCHAR fname[MAX_PATH];
 	TCHAR directory[MAX_PATH];
@@ -361,6 +362,7 @@ VOID FindPrefixAndSuffix(LPTSTR strIN, LPTSTR szPrefix, LPTSTR szSuffix)
 	TCHAR * szSearch;	
 	TCHAR * szSearch1;
 	TCHAR * szSearch2;
+	TCHAR * szSearch3;
 	/* number of quotes in the string */
 	INT nQuotes = 0;
 	/* used in for loops */
@@ -394,9 +396,12 @@ VOID FindPrefixAndSuffix(LPTSTR strIN, LPTSTR szPrefix, LPTSTR szSuffix)
 		_tcscpy(szSuffix,szSearch);
 		/* Find the one closest to end */
 		szSearch1 = _tcsrchr(str, _T('\"'));
-		szSearch2 = _tcsrchr(str, _T('\\'));		
+		szSearch2 = _tcsrchr(str, _T('\\'));
+		szSearch3 = _tcsrchr(str, _T('.'));
 		if(szSearch2 != NULL && _tcslen(szSearch1) > _tcslen(szSearch2))
 			szSearch = szSearch2;
+		else if(szSearch3 != NULL && _tcslen(szSearch1) > _tcslen(szSearch3))
+			szSearch = szSearch3;
 		else
 			szSearch = szSearch1;
 		/* Move one char past */
@@ -436,9 +441,12 @@ VOID FindPrefixAndSuffix(LPTSTR strIN, LPTSTR szPrefix, LPTSTR szSuffix)
 		/* Find the closest to the end space or \ */
 		_tcscpy(str,strIN);
 		szSearch1 = _tcsrchr(str, _T(' '));
-		szSearch2 = _tcsrchr(str, _T('\\'));		
+		szSearch2 = _tcsrchr(str, _T('\\'));
+		szSearch3 = _tcsrchr(str, _T('/'));
 		if(szSearch2 != NULL && _tcslen(szSearch1) > _tcslen(szSearch2))
 			szSearch = szSearch2;
+		else if(szSearch3 != NULL && _tcslen(szSearch1) > _tcslen(szSearch3))
+			szSearch = szSearch3;
 		else
 			szSearch = szSearch1;
 		szSearch++;		

base/shell/cmd/cmd.c
@@ -1172,7 +1172,7 @@ GetEnvVarOrSpecial ( LPCTSTR varName )
 		GetSystemTime(&t);
 		_sntprintf ( ret, retlen, _T("%02d%c%02d%c%02d%c%02d"),
 			t.wHour, cTimeSeparator, t.wMinute, cTimeSeparator,
-			t.wSecond, cDecimalSeparator, t.wMilliseconds );
+			t.wSecond, cDecimalSeparator, t.wMilliseconds / 10);
 		return ret;
 	}
 	/* %DATE% */

base/shell/cmd/cmdinput.c
@@ -139,8 +139,10 @@ VOID ReadCommand (LPTSTR str, INT maxlen)
 	TCHAR  ch;
 	BOOL bContinue=FALSE;/*is TRUE the second case will not be executed*/
     BOOL bReturn = FALSE;
-	TCHAR szPath[MAX_PATH];
     BOOL bCharInput;
+    #ifdef FEATURE_4NT_FILENAME_COMPLETION
+    TCHAR szPath[MAX_PATH];
+    #endif
 
 	/* get screen size */
 	GetScreenSize (&maxx, &maxy);

commit d12169b496a1f6be0724bc724905e169c7311b87

CMD: Small fixes.
- Fix completion when path contains "dots", as in:
  "some(file).ext", and you remove the final quote (or the quote and
  "ext") and you attempt to complete the file name.
  See CORE-8623 and CORE-1901 (bug introduced in r25896 / 54cf74f).
- Add some annotations to help me in refactoring the code.
- Add other handling of '/'.

base/shell/cmd/filecomp.c
@@ -553,7 +553,7 @@ CompleteFilenameBash(
     if (RestartCompletion)
     {
         INT curplace = 0;
-        BOOL  found_dot = FALSE;
+        BOOL found_dot = FALSE;
         TCHAR path[MAX_PATH];
 
         /* extract directory from word */
@@ -716,13 +716,14 @@ FindPrefixAndSuffix_Cmd(
     }
 
     /* Find the prefix and suffix */
+
+    /* Odd number of quotes */
     if (nQuotes % 2)
     {
         /*
          * Odd number of quotes. Just start from the last quote.
          * This is the way MS does it, and is an easy way out.
          */
-
         ASSERT(InsideQuotes);
 
         szSearch = str + LastQuote; // _tcsrchr(str, _T('\"'));
@@ -733,8 +734,8 @@ FindPrefixAndSuffix_Cmd(
 
         /* Find the one closest to end */
         szSearch1 = _tcsrchr(szSearch + 1, _T('\\'));
-        szSearch2 = _tcsrchr(szSearch + 1, _T('.'));
-        if (szSearch1 != NULL) // '\\' has precedence over '.'
+        szSearch2 = _tcsrchr(szSearch + 1, _T('/'));
+        if (szSearch1 != NULL) // '\\' has precedence over '/'
             szSearch = szSearch1;
         else if (szSearch2 != NULL)
             szSearch = szSearch2;
@@ -748,10 +749,12 @@ FindPrefixAndSuffix_Cmd(
         return;
     }
 
-    /*
-     * Even number of quotes.
-     */
+    /* Even number of quotes */
 
+    // szSearch = _tcsrchr(str, _T(' '));
+
+    /* No spaces */
+    // if (!szSearch)
     if (!_tcschr(str, _T(' ')))
     {
         /* No spaces, everything goes to Suffix */
@@ -760,7 +763,13 @@ FindPrefixAndSuffix_Cmd(
         // _tcsncpy(szPrefix, str, charcount);
 
         /* Look for a slash just in case */
-        szSearch = _tcsrchr(str, _T('\\'));
+        szSearch1 = _tcsrchr(str, _T('\\'));
+        szSearch2 = _tcsrchr(str, _T('/'));
+        if (szSearch1 != NULL) // '\\' has precedence over '/'
+            szSearch = szSearch1;
+        else if (szSearch2 != NULL)
+            szSearch = szSearch2;
+
         if (szSearch)
         {
             /* Move one char past */
@@ -772,13 +781,14 @@ FindPrefixAndSuffix_Cmd(
         }
         else
         {
-            // We know that "prefix" starts from 'str' to 'str' (i.e. empty)
+            // We know that "prefix" is empty
             szPrefix[0] = _T('\0');
         }
 
         return;
     }
 
+    /* Zero quotes */
     if (!nQuotes)
     {
         /* No quotes, and there is a space. Take it after the last space. */
@@ -804,9 +814,11 @@ FindPrefixAndSuffix_Cmd(
         return;
     }
 
-    /* All else fails and there is a lot of quotes, spaces and |
-       Then we search through and find the last space or \ that is
-        not inside a quotes */
+    /*
+     * All else fails: we have an even and non-zero number of quotes
+     * and spaces. Then we search through and find the last space or \
+     * that is not inside quotes.
+     */
     InsideQuotes = FALSE;
     for (i = 0; i < charcount /*_tcslen(str)*/; i++)
     {
@@ -814,7 +826,7 @@ FindPrefixAndSuffix_Cmd(
             InsideQuotes = !InsideQuotes;
         if (str[i] == _T(' ') && !InsideQuotes)
             SBreak = i;
-        if ((str[i] == _T(' ') || str[i] == _T('\\')) && !InsideQuotes)
+        if ((str[i] == _T(' ') || str[i] == _T('\\') || str[i] == _T('/')) && !InsideQuotes)
             PBreak = i;
     }
     SBreak++;
@@ -824,13 +836,12 @@ FindPrefixAndSuffix_Cmd(
 
     _tcsncpy(szPrefix, str, PBreak);
     szPrefix[PBreak] = _T('\0');
-    i = PBreak; // _tcslen(szPrefix);
-    if (i >= 2 && szPrefix[i - 2] == _T('\"') && szPrefix[i - 1] != _T(' '))
+    if (PBreak >= 2 && szPrefix[PBreak - 2] == _T('\"') && szPrefix[PBreak - 1] != _T(' '))
     {
         /* need to remove the " right before a \ at the end to
            allow the next stuff to stay inside one set of quotes
            otherwise you would have multiple sets of quotes */
-        _tcscpy(&szPrefix[i - 2], _T("\\"));
+        _tcscpy(&szPrefix[PBreak - 2], _T("\\"));
     }
 }
 