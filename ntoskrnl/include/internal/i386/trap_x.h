a286748beefa7c3149adb5e3e9d42277134efd18
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2021-05-22 19:50:00 +0200

    [NTOS:KE] Avoid unnecessary stack variables in the enter-trap stubs (give also "unused variable" warnings in Release builds).

--- ntoskrnl/include/internal/i386/trap_x.h
+++ ntoskrnl/include/internal/i386/trap_x.h
@@ -344,12 +344,9 @@ FORCEINLINE
 VOID
 KiEnterV86Trap(IN PKTRAP_FRAME TrapFrame)
 {
-    PVOID ExceptionList;
-
     /* Check exception list */
-    ExceptionList = KeGetPcr()->NtTib.ExceptionList;
     ASSERTMSG("V86 trap handler must not register an SEH frame\n",
-              ExceptionList == TrapFrame->ExceptionList);
+              KeGetPcr()->NtTib.ExceptionList == TrapFrame->ExceptionList);
 
     /* Save DR7 and check for debugging */
     TrapFrame->Dr7 = __readdr(7);
@@ -367,12 +364,11 @@ FORCEINLINE
 VOID
 KiEnterInterruptTrap(IN PKTRAP_FRAME TrapFrame)
 {
-    PVOID ExceptionList;
-
-    /* Check exception list and terminate it */
-    ExceptionList = KeGetPcr()->NtTib.ExceptionList;
+    /* Check exception list */
     ASSERTMSG("Interrupt handler must not register an SEH frame\n",
-              ExceptionList == TrapFrame->ExceptionList);
+              KeGetPcr()->NtTib.ExceptionList == TrapFrame->ExceptionList);
+
+    /* Terminate the exception list */
     KeGetPcr()->NtTib.ExceptionList = EXCEPTION_CHAIN_END;
 
     /* Default to debugging disabled */
@@ -401,12 +397,9 @@ FORCEINLINE
 VOID
 KiEnterTrap(IN PKTRAP_FRAME TrapFrame)
 {
-    PVOID ExceptionList;
-
     /* Check exception list */
-    ExceptionList = KeGetPcr()->NtTib.ExceptionList;
     ASSERTMSG("Trap handler must not register an SEH frame\n",
-              ExceptionList == TrapFrame->ExceptionList);
+              KeGetPcr()->NtTib.ExceptionList == TrapFrame->ExceptionList);
 
     /* Default to debugging disabled */
     TrapFrame->Dr7 = 0;
