fa9c6ba7dc60483ca99801e77c965b5842792ee2
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2022-11-07 20:09:12 +0100

    addendum tstartup copyright notice

--- sdk/lib/nt/_tstartup.h
+++ sdk/lib/nt/_tstartup.h
@@ -2,7 +2,7 @@
  * PROJECT:     ReactOS Native NT Runtime Library
  * LICENSE:     GPL-2.0+ (https://spdx.org/licenses/GPL-2.0+)
  * PURPOSE:     Entry point for Native NT Programs
- * COPYRIGHT:   Copyright 2007 Alex Ionescu (alex@relsoft.net)
+ * COPYRIGHT:   Copyright 2007 Alex Ionescu <alex@relsoft.net>
  *              Copyright 2022 Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
  */
 
