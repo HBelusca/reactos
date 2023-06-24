7f1f674b1183f5d7b309f8b948ec97465fa2f85a
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2023-06-24 21:46:40 +0200

    [FREELDR] Reimplement FldrCreateSystemKey around FldrCreateComponentKey.
    Also, add a parameter to specify whether the system is ARC-compatible
    or not (sets the Component type to either ArcSystem or MaximumType).

--- boot/freeldr/freeldr/arch/i386/pc/machpc.c
+++ boot/freeldr/freeldr/arch/i386/pc/machpc.c
@@ -1858,7 +1858,7 @@ PcHwDetect(
 
     /* Create the 'System' key */
     // TODO: Discover and set the other machine types
-    FldrCreateSystemKey(&SystemKey, "AT/AT COMPATIBLE");
+    FldrCreateSystemKey(&SystemKey, FALSE, "AT/AT COMPATIBLE");
 
     GetHarddiskConfigurationData = PcGetHarddiskConfigurationData;
     FindPciBios = PcFindPciBios;
