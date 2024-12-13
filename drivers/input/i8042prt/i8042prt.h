59c0766fa0f13fecead847e4e23f7e961107e39a
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2024-12-13 17:37:19 +0100

    [I8042PRT] i8042prt.h, keyboard.c, registry.c: TAB -> 4-space formatting
    Effectively reverts commit 0560eef6e (r39553) that reverted commit
    a2feddeab (r39517).

--- drivers/input/i8042prt/i8042prt.h
+++ drivers/input/i8042prt/i8042prt.h
@@ -1,5 +1,5 @@
-#ifndef _I8042PRT_PCH_
-#define _I8042PRT_PCH_
+#ifndef _I8042PRT_H_
+#define _I8042PRT_H_
 
 #include <ntifs.h>
 #include <kbdmou.h>
@@ -13,48 +13,48 @@
 
 typedef enum
 {
-	dsStopped,
-	dsStarted,
-	dsPaused,
-	dsRemoved,
-	dsSurpriseRemoved
+    dsStopped,
+    dsStarted,
+    dsPaused,
+    dsRemoved,
+    dsSurpriseRemoved
 } DEVICE_STATE;
 
 typedef struct _I8042_SETTINGS
 {
-	/* Registry settings */
-	ULONG KeyboardDataQueueSize;           /* done */
-	UNICODE_STRING KeyboardDeviceBaseName;
-	ULONG MouseDataQueueSize;              /* done */
-	ULONG MouseResolution;
-	ULONG MouseSynchIn100ns;
-	ULONG NumberOfButtons;
-	UNICODE_STRING PointerDeviceBaseName;
-	ULONG PollStatusIterations;            /* done */
-	ULONG OverrideKeyboardType;
-	ULONG OverrideKeyboardSubtype;
-	ULONG PollingIterations;               /* done */
-	ULONG PollingIterationsMaximum;
-	ULONG ResendIterations;                /* done */
-	ULONG SampleRate;
-	ULONG CrashOnCtrlScroll;               /* done */
+    /* Registry settings */
+    ULONG KeyboardDataQueueSize;           /* done */
+    UNICODE_STRING KeyboardDeviceBaseName;
+    ULONG MouseDataQueueSize;              /* done */
+    ULONG MouseResolution;
+    ULONG MouseSynchIn100ns;
+    ULONG NumberOfButtons;
+    UNICODE_STRING PointerDeviceBaseName;
+    ULONG PollStatusIterations;            /* done */
+    ULONG OverrideKeyboardType;
+    ULONG OverrideKeyboardSubtype;
+    ULONG PollingIterations;               /* done */
+    ULONG PollingIterationsMaximum;
+    ULONG ResendIterations;                /* done */
+    ULONG SampleRate;
+    ULONG CrashOnCtrlScroll;               /* done */
 } I8042_SETTINGS, *PI8042_SETTINGS;
 
 typedef enum _MOUSE_TIMEOUT_STATE
 {
-	NoChange,
-	TimeoutStart,
-	TimeoutCancel
+    NoChange,
+    TimeoutStart,
+    TimeoutCancel
 } MOUSE_TIMEOUT_STATE, *PMOUSE_TIMEOUT_STATE;
 
 typedef struct _INTERRUPT_DATA
 {
-	PKINTERRUPT Object;
-	ULONG Vector;
-	KIRQL Dirql;
-	KINTERRUPT_MODE InterruptMode;
-	BOOLEAN ShareInterrupt;
-	KAFFINITY Affinity;
+    PKINTERRUPT Object;
+    ULONG Vector;
+    KIRQL Dirql;
+    KINTERRUPT_MODE InterruptMode;
+    BOOLEAN ShareInterrupt;
+    KAFFINITY Affinity;
 } INTERRUPT_DATA, *PINTERRUPT_DATA;
 
 #define WHEEL_DELTA 120
@@ -76,132 +76,132 @@ typedef struct _I8042_MOUSE_EXTENSION *PI8042_MOUSE_EXTENSION;
 
 typedef struct _PORT_DEVICE_EXTENSION
 {
-	PUCHAR DataPort;    /* Usually 0x60 */
-	PUCHAR ControlPort; /* Usually 0x64 */
-	I8042_SETTINGS Settings;
-	ULONG Flags;
+    PUCHAR DataPort;    /* Usually 0x60 */
+    PUCHAR ControlPort; /* Usually 0x64 */
+    I8042_SETTINGS Settings;
+    ULONG Flags;
 
-	PI8042_KEYBOARD_EXTENSION KeyboardExtension;
-	INTERRUPT_DATA KeyboardInterrupt;
-	PI8042_MOUSE_EXTENSION MouseExtension;
-	INTERRUPT_DATA MouseInterrupt;
-	PKINTERRUPT HighestDIRQLInterrupt;
-	KSPIN_LOCK SpinLock;
-	KIRQL HighestDirql;
+    PI8042_KEYBOARD_EXTENSION KeyboardExtension;
+    INTERRUPT_DATA KeyboardInterrupt;
+    PI8042_MOUSE_EXTENSION MouseExtension;
+    INTERRUPT_DATA MouseInterrupt;
+    PKINTERRUPT HighestDIRQLInterrupt;
+    KSPIN_LOCK SpinLock;
+    KIRQL HighestDirql;
 
-	OUTPUT_PACKET Packet;
-	ULONG PacketResends;
-	BOOLEAN PacketComplete;
-	NTSTATUS PacketResult;
-	UCHAR PacketBuffer[16];
-	UCHAR PacketPort;
+    OUTPUT_PACKET Packet;
+    ULONG PacketResends;
+    BOOLEAN PacketComplete;
+    NTSTATUS PacketResult;
+    UCHAR PacketBuffer[16];
+    UCHAR PacketPort;
 
-	PIRP CurrentIrp;
-	PDEVICE_OBJECT CurrentIrpDevice;
+    PIRP CurrentIrp;
+    PDEVICE_OBJECT CurrentIrpDevice;
 } PORT_DEVICE_EXTENSION, *PPORT_DEVICE_EXTENSION;
 
 typedef struct _I8042_DRIVER_EXTENSION
 {
-	UNICODE_STRING RegistryPath;
+    UNICODE_STRING RegistryPath;
 
-	PORT_DEVICE_EXTENSION Port;
-	LIST_ENTRY DeviceListHead;
-	KSPIN_LOCK DeviceListLock;
+    PORT_DEVICE_EXTENSION Port;
+    LIST_ENTRY DeviceListHead;
+    KSPIN_LOCK DeviceListLock;
 } I8042_DRIVER_EXTENSION, *PI8042_DRIVER_EXTENSION;
 
 typedef enum _I8042_DEVICE_TYPE
 {
-	Unknown,
-	Keyboard,
-	Mouse,
-	PhysicalDeviceObject
+    Unknown,
+    Keyboard,
+    Mouse,
+    PhysicalDeviceObject
 } I8042_DEVICE_TYPE, *PI8042_DEVICE_TYPE;
 
 typedef struct _FDO_DEVICE_EXTENSION
 {
-	I8042_DEVICE_TYPE Type;
-	// Linkage in I8042_DRIVER_EXTENSION.DeviceListHead
-	LIST_ENTRY ListEntry;
-	// Associated device object (FDO)
-	PDEVICE_OBJECT Fdo;
-	// Associated device object (PDO)
-	PDEVICE_OBJECT Pdo;
-	// Lower device object
-	PDEVICE_OBJECT LowerDevice;
-	// Current state of the driver
-	DEVICE_STATE PnpState;
+    I8042_DEVICE_TYPE Type;
+    // Linkage in I8042_DRIVER_EXTENSION.DeviceListHead
+    LIST_ENTRY ListEntry;
+    // Associated device object (FDO)
+    PDEVICE_OBJECT Fdo;
+    // Associated device object (PDO)
+    PDEVICE_OBJECT Pdo;
+    // Lower device object
+    PDEVICE_OBJECT LowerDevice;
+    // Current state of the driver
+    DEVICE_STATE PnpState;
 
-	PPORT_DEVICE_EXTENSION PortDeviceExtension;
+    PPORT_DEVICE_EXTENSION PortDeviceExtension;
 } FDO_DEVICE_EXTENSION, *PFDO_DEVICE_EXTENSION;
 
 typedef struct _I8042_KEYBOARD_EXTENSION
 {
-	FDO_DEVICE_EXTENSION Common;
-	CONNECT_DATA KeyboardData;
-	INTERNAL_I8042_HOOK_KEYBOARD KeyboardHook; /* FIXME: IsrWritePort ignored */
-	KDPC DpcKeyboard;
+    FDO_DEVICE_EXTENSION Common;
+    CONNECT_DATA KeyboardData;
+    INTERNAL_I8042_HOOK_KEYBOARD KeyboardHook; /* FIXME: IsrWritePort ignored */
+    KDPC DpcKeyboard;
 
-	KEYBOARD_ATTRIBUTES KeyboardAttributes;
+    KEYBOARD_ATTRIBUTES KeyboardAttributes;
 
-	KEYBOARD_INDICATOR_PARAMETERS KeyboardIndicators;
+    KEYBOARD_INDICATOR_PARAMETERS KeyboardIndicators;
 
-	KEYBOARD_SCAN_STATE KeyboardScanState;
-	BOOLEAN KeyComplete;
-	PKEYBOARD_INPUT_DATA KeyboardBuffer;
-	ULONG KeysInBuffer;
+    KEYBOARD_SCAN_STATE KeyboardScanState;
+    BOOLEAN KeyComplete;
+    PKEYBOARD_INPUT_DATA KeyboardBuffer;
+    ULONG KeysInBuffer;
 
-	/* Power keys items */
-	ULONG ReportedCaps;
-	ULONG NewCaps;
-	ULONG LastPowerKey;
-	UNICODE_STRING PowerInterfaceName;
-	PIO_WORKITEM PowerWorkItem;
-	PIRP PowerIrp;
+    /* Power keys items */
+    ULONG ReportedCaps;
+    ULONG NewCaps;
+    ULONG LastPowerKey;
+    UNICODE_STRING PowerInterfaceName;
+    PIO_WORKITEM PowerWorkItem;
+    PIRP PowerIrp;
 
-	/* Debug items */
-	ULONG ComboPosition;
-	PIO_WORKITEM DebugWorkItem;
-	BOOLEAN TabPressed;
+    /* Debug items */
+    ULONG ComboPosition;
+    PIO_WORKITEM DebugWorkItem;
+    BOOLEAN TabPressed;
 } I8042_KEYBOARD_EXTENSION;
 
 typedef enum _I8042_MOUSE_TYPE
 {
-	GenericPS2,
-	Intellimouse,
-	IntellimouseExplorer,
-	Ps2pp
+    GenericPS2,
+    Intellimouse,
+    IntellimouseExplorer,
+    Ps2pp
 } I8042_MOUSE_TYPE, *PI8042_MOUSE_TYPE;
 
 typedef struct _I8042_MOUSE_EXTENSION
 {
-	FDO_DEVICE_EXTENSION Common;
-	CONNECT_DATA MouseData;
-	INTERNAL_I8042_HOOK_MOUSE MouseHook;
-	KDPC DpcMouse;
+    FDO_DEVICE_EXTENSION Common;
+    CONNECT_DATA MouseData;
+    INTERNAL_I8042_HOOK_MOUSE MouseHook;
+    KDPC DpcMouse;
 
-	MOUSE_ATTRIBUTES MouseAttributes;
+    MOUSE_ATTRIBUTES MouseAttributes;
 
-	MOUSE_STATE MouseState;
-	BOOLEAN MouseComplete;
-	MOUSE_RESET_SUBSTATE MouseResetState;
-	PMOUSE_INPUT_DATA MouseBuffer;
-	ULONG MouseInBuffer;
-	USHORT MouseButtonState;
-	ULARGE_INTEGER MousePacketStartTime;
+    MOUSE_STATE MouseState;
+    BOOLEAN MouseComplete;
+    MOUSE_RESET_SUBSTATE MouseResetState;
+    PMOUSE_INPUT_DATA MouseBuffer;
+    ULONG MouseInBuffer;
+    USHORT MouseButtonState;
+    ULARGE_INTEGER MousePacketStartTime;
 
-	KTIMER TimerMouseTimeout;
-	KDPC DpcMouseTimeout;
-	MOUSE_TIMEOUT_STATE MouseTimeoutState;
-	BOOLEAN MouseTimeoutActive;
+    KTIMER TimerMouseTimeout;
+    KDPC DpcMouseTimeout;
+    MOUSE_TIMEOUT_STATE MouseTimeoutState;
+    BOOLEAN MouseTimeoutActive;
 
-	UCHAR MouseLogiBuffer[3];
-	I8042_MOUSE_TYPE MouseType;
+    UCHAR MouseLogiBuffer[3];
+    I8042_MOUSE_TYPE MouseType;
 } I8042_MOUSE_EXTENSION;
 
 typedef struct _I8042_HOOK_WORKITEM
 {
-	PIO_WORKITEM WorkItem;
-	PIRP Irp;
+    PIO_WORKITEM WorkItem;
+    PIRP Irp;
 } I8042_HOOK_WORKITEM, *PI8042_HOOK_WORKITEM;
 
 /*-----------------------------------------------------
@@ -307,9 +307,9 @@ DRIVER_DISPATCH i8042Close;
 
 NTSTATUS NTAPI
 i8042SynchWritePortKbd(
-	IN PVOID Context,
-	IN UCHAR Value,
-	IN BOOLEAN WaitForAck);
+    IN PVOID Context,
+    IN UCHAR Value,
+    IN BOOLEAN WaitForAck);
 
 DRIVER_STARTIO i8042KbdStartIo;
 
@@ -325,16 +325,16 @@ DRIVER_ADD_DEVICE i8042AddDevice;
 
 BOOLEAN
 i8042PacketIsr(
-	IN PPORT_DEVICE_EXTENSION DeviceExtension,
-	IN UCHAR Output);
+    IN PPORT_DEVICE_EXTENSION DeviceExtension,
+    IN UCHAR Output);
 
 NTSTATUS
 i8042StartPacket(
-	IN PPORT_DEVICE_EXTENSION DeviceExtension,
-	IN PFDO_DEVICE_EXTENSION FdoDeviceExtension,
-	IN PUCHAR Bytes,
-	IN ULONG ByteCount,
-	IN PIRP Irp);
+    IN PPORT_DEVICE_EXTENSION DeviceExtension,
+    IN PFDO_DEVICE_EXTENSION FdoDeviceExtension,
+    IN PUCHAR Bytes,
+    IN ULONG ByteCount,
+    IN PIRP Irp);
 
 /* misc.c */
 
@@ -342,25 +342,25 @@ DRIVER_DISPATCH ForwardIrpAndForget;
 
 NTSTATUS
 DuplicateUnicodeString(
-	IN ULONG Flags,
-	IN PCUNICODE_STRING SourceString,
-	OUT PUNICODE_STRING DestinationString);
+    IN ULONG Flags,
+    IN PCUNICODE_STRING SourceString,
+    OUT PUNICODE_STRING DestinationString);
 
 /* mouse.c */
 
 VOID
 i8042MouHandle(
-	IN PI8042_MOUSE_EXTENSION DeviceExtension,
-	IN UCHAR Output);
+    IN PI8042_MOUSE_EXTENSION DeviceExtension,
+    IN UCHAR Output);
 
 VOID
 i8042MouHandleButtons(
-	IN PI8042_MOUSE_EXTENSION DeviceExtension,
-	IN USHORT Mask);
+    IN PI8042_MOUSE_EXTENSION DeviceExtension,
+    IN USHORT Mask);
 
 NTSTATUS
 i8042MouInitialize(
-	IN PI8042_MOUSE_EXTENSION DeviceExtension);
+    IN PI8042_MOUSE_EXTENSION DeviceExtension);
 
 DRIVER_DISPATCH i8042MouInternalDeviceControl;
 
@@ -370,9 +370,9 @@ KSERVICE_ROUTINE i8042MouInterruptService;
 
 BOOLEAN
 i8042ChangeMode(
-	IN PPORT_DEVICE_EXTENSION DeviceExtension,
-	IN UCHAR FlagsToDisable,
-	IN UCHAR FlagsToEnable);
+    IN PPORT_DEVICE_EXTENSION DeviceExtension,
+    IN UCHAR FlagsToDisable,
+    IN UCHAR FlagsToEnable);
 
 _Dispatch_type_(IRP_MJ_PNP)
 DRIVER_DISPATCH i8042Pnp;
@@ -380,66 +380,66 @@ DRIVER_DISPATCH i8042Pnp;
 /* ps2pp.c */
 VOID
 i8042MouHandlePs2pp(
-	IN PI8042_MOUSE_EXTENSION DeviceExtension,
-	IN UCHAR Input);
+    IN PI8042_MOUSE_EXTENSION DeviceExtension,
+    IN UCHAR Input);
 
 /* readwrite.c */
 
 VOID
 i8042Flush(
-	IN PPORT_DEVICE_EXTENSION DeviceExtension);
+    IN PPORT_DEVICE_EXTENSION DeviceExtension);
 
 BOOLEAN
 i8042IsrWritePort(
-	IN PPORT_DEVICE_EXTENSION DeviceExtension,
-	IN UCHAR Value,
-	IN UCHAR SelectCmd OPTIONAL);
+    IN PPORT_DEVICE_EXTENSION DeviceExtension,
+    IN UCHAR Value,
+    IN UCHAR SelectCmd OPTIONAL);
 
 NTSTATUS
 i8042ReadData(
-	IN PPORT_DEVICE_EXTENSION DeviceExtension,
-	IN UCHAR StatusFlags,
-	OUT PUCHAR Data);
+    IN PPORT_DEVICE_EXTENSION DeviceExtension,
+    IN UCHAR StatusFlags,
+    OUT PUCHAR Data);
 #define i8042ReadKeyboardData(DeviceExtension, Data) \
-	i8042ReadData(DeviceExtension, KBD_OBF, Data)
+    i8042ReadData(DeviceExtension, KBD_OBF, Data)
 #define i8042ReadMouseData(DeviceExtension, Data) \
-	i8042ReadData(DeviceExtension, MOU_OBF, Data)
+    i8042ReadData(DeviceExtension, MOU_OBF, Data)
 
 NTSTATUS
 i8042ReadDataWait(
-	IN PPORT_DEVICE_EXTENSION DeviceExtension,
-	OUT PUCHAR Data);
+    IN PPORT_DEVICE_EXTENSION DeviceExtension,
+    OUT PUCHAR Data);
 
 NTSTATUS
 i8042ReadStatus(
-	IN PPORT_DEVICE_EXTENSION DeviceExtension,
-	OUT PUCHAR Status);
+    IN PPORT_DEVICE_EXTENSION DeviceExtension,
+    OUT PUCHAR Status);
 
 NTSTATUS NTAPI
 i8042SynchReadPort(
-	IN PVOID Context,
-	OUT PUCHAR Value,
-	IN BOOLEAN WaitForAck);
+    IN PVOID Context,
+    OUT PUCHAR Value,
+    IN BOOLEAN WaitForAck);
 
 NTSTATUS NTAPI
 i8042SynchWritePort(
-	IN PPORT_DEVICE_EXTENSION DeviceExtension,
-	IN UCHAR Port,
-	IN UCHAR Value,
-	IN BOOLEAN WaitForAck);
+    IN PPORT_DEVICE_EXTENSION DeviceExtension,
+    IN UCHAR Port,
+    IN UCHAR Value,
+    IN BOOLEAN WaitForAck);
 
 BOOLEAN
 i8042Write(
-	IN PPORT_DEVICE_EXTENSION DeviceExtension,
-	IN PUCHAR addr,
-	IN UCHAR data);
+    IN PPORT_DEVICE_EXTENSION DeviceExtension,
+    IN PUCHAR addr,
+    IN UCHAR data);
 
 /* registry.c */
 
 NTSTATUS
 ReadRegistryEntries(
-	IN PUNICODE_STRING RegistryPath,
-	OUT PI8042_SETTINGS Settings);
+    IN PUNICODE_STRING RegistryPath,
+    OUT PI8042_SETTINGS Settings);
 
 /* hwhacks.c */
 
@@ -456,4 +456,4 @@ enum _FLAGS
 
 extern ULONG i8042HwFlags;
 
-#endif /* _I8042PRT_PCH_ */
+#endif /* _I8042PRT_H_ */
