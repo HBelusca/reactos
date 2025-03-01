7113126c604faf1afedd9303f95b4610dec58fc6
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2025-03-01 19:53:57 +0100

    test bitmaps for btani2k

--- ntoskrnl/inbv/btani2k.c
+++ ntoskrnl/inbv/btani2k.c
@@ -16,20 +16,25 @@
 #define VID_SCROLL_AREA_LEFT        8   // BOOTCHAR_WIDTH
 #define VID_SCROLL_AREA_TOP         80  /* for header v2 */ // 125 for header v1
 #define VID_SCROLL_AREA_RIGHT       (SCREEN_WIDTH - 1 - 8)
-#define VID_SCROLL_AREA_BOTTOM      400 // (480 - 65 - BOOTCHAR_HEIGHT - 2) // (400 + 13)
+#define VID_SCROLL_AREA_BOTTOM      (400 + 13/*9*/) // (480 - 65 - BOOTCHAR_HEIGHT - 2) // 400
+
+#define VID_ROTBAR_TOP              79 /* for header v2 */ // 122 /*for header v1*/ // (SCREEN_HEIGHT-65) // (SCREEN_HEIGHT-6)
 
 #define VID_PROGRESS_BAR_LEFT       165
 #define VID_PROGRESS_BAR_TOP        446
 #define VID_PROGRESS_BAR_WIDTH      366
 #define VID_PROGRESS_BAR_HEIGHT     19
 
-/* 16px space between shutdown logo and message */
-#define VID_SHUTDOWN_LOGO_LEFT    225
-#define VID_SHUTDOWN_LOGO_TOP     114
-#define VID_SHUTDOWN_MSG_LEFT     213
-#define VID_SHUTDOWN_MSG_TOP      354
+#define VID_HIBER_PRGBAR_LEFT       210
+#define VID_HIBER_PRGBAR_TOP        270
 
-#define VID_FOOTER_BG_TOP        (SCREEN_HEIGHT - 59)
+/* 16px space between shutdown logo and message */
+#define VID_SHUTDOWN_LOGO_LEFT      225
+#define VID_SHUTDOWN_LOGO_TOP       114
+// #define VID_SHUTDOWN_MSG_LEFT       213
+// #define VID_SHUTDOWN_MSG_TOP        354
+
+#define VID_FOOTER_BG_TOP        (SCREEN_HEIGHT - 66)
 
 
 /* GLOBALS *******************************************************************/
@@ -42,18 +47,12 @@
  */
 // #define REACTOS_SKUS
 
-/*
- * Enable this define when Inbv will support rotating progress bar.
- */
-#define BOOT_ROTBAR_IMPLEMENTED
-
 #define BOOTANIM_BY_DPC
 #include "btanihlp.c"
 
 extern ULONG ProgressBarLeft, ProgressBarTop;
 extern BOOLEAN ShowProgressBar;
 
-#ifdef BOOT_ROTBAR_IMPLEMENTED
 /*
  * Values for PltRotBarStatus:
  * - PltRotBarStatus == 1, do palette fading-in (done elsewhere in ReactOS);
@@ -63,24 +62,19 @@ extern BOOLEAN ShowProgressBar;
  */
 typedef enum _ROT_BAR_STATUS
 {
+    // RBS_PAUSE = 0,
     RBS_FADEIN = 1,
     RBS_ANIMATE,
     RBS_STOP_ANIMATE,
     RBS_STATUS_MAX
 } ROT_BAR_STATUS;
 
-typedef enum _ROT_BAR_TYPE
-{
-    RB_UNSPECIFIED,
-    RB_SCREEN,
-    RB_TEXT
-} ROT_BAR_TYPE;
+static ULONG /*RotBarLeft = 0,*/ RotBarTop = 0;
 
 static BOOLEAN AnimationActive = FALSE;
-static ROT_BAR_TYPE RotBarSelection = RB_UNSPECIFIED;
 static ROT_BAR_STATUS PltRotBarStatus = 0;
 static UCHAR RotLineBuffer[SCREEN_WIDTH * 6];
-#endif
+
 
 /* FUNCTIONS *****************************************************************/
 
@@ -138,6 +132,8 @@ BootThemeTickProgressBar(
 
     /* Compute fill count */
     FillCount = VID_PROGRESS_BAR_WIDTH * SubPercentTimes100 / (100 * 100);
+
+    InbvAcquireLock();
 
     /* If 0%, draw also the left round corner */
     if (SubPercentTimes100 <= 10*100)
@@ -225,16 +221,17 @@ BootThemeTickProgressBar(
                           ProgressBarTop - 6 + VID_PROGRESS_BAR_HEIGHT,
                           BV_COLOR_WHITE);
     }
+
+    InbvReleaseLock();
 }
 
-#ifdef BOOT_ROTBAR_IMPLEMENTED
-VOID // BOOLEAN
+VOID
 NTAPI
 BootAnimUpdate(
     _In_ PBOOT_ANIM_CTX BootAnimCtx)
 {
     // Boot animation context variables
-    static ULONG Y, Index = 0;
+    static ULONG Index = 0;
 
     UNREFERENCED_PARAMETER(BootAnimCtx);
 
@@ -244,40 +241,32 @@ BootAnimUpdate(
     if (PltRotBarStatus == RBS_STOP_ANIMATE)
     {
         /* Stop the animation */
-        return; // return FALSE;
+        // BootAnimCtx->Terminate = TRUE;
+        return;
     }
 
-    if (RotBarSelection != RB_UNSPECIFIED)
+    if (PltRotBarStatus == RBS_FADEIN)
+    {
+        /* We are starting the animation, perform first initialization */
+        Index = 0;
+        PltRotBarStatus = RBS_ANIMATE;
+    }
+    else if (PltRotBarStatus == RBS_ANIMATE) // (PltRotBarStatus != 0)
     {
         Index %= SCREEN_WIDTH;
 
-        if (RotBarSelection == RB_SCREEN)
-            Y = SCREEN_HEIGHT-6;
-        else // if (RotBarSelection == RB_TEXT)
-            Y = 79-6 /* for header v2 */; // 122-6 /*for header v1*/; // SCREEN_HEIGHT-65; // SCREEN_HEIGHT-6;
-
         /* Right part */
-        VidBufferToScreenBlt(RotLineBuffer, Index, Y, SCREEN_WIDTH - Index, 6, SCREEN_WIDTH);
+        VidBufferToScreenBlt(RotLineBuffer, Index,
+                             RotBarTop, SCREEN_WIDTH - Index, 6, SCREEN_WIDTH);
         if (Index > 0)
         {
             /* Left part */
-            VidBufferToScreenBlt(RotLineBuffer + (SCREEN_WIDTH - Index) / 2, 0, Y, Index - 2, 6, SCREEN_WIDTH);
+            VidBufferToScreenBlt(RotLineBuffer + (SCREEN_WIDTH - Index) / 2, 0,
+                                 RotBarTop, Index - 2, 6, SCREEN_WIDTH);
         }
         Index += 4;
     }
-
-    return; // TRUE;
 }
-
-CODE_SEG("INIT")
-static
-VOID
-InbvRotBarInit(VOID)
-{
-    PltRotBarStatus = RBS_FADEIN;
-    /* Perform other initialization if needed */
-}
-#endif
 
 CODE_SEG("INIT")
 VOID
@@ -285,18 +274,24 @@ NTAPI
 DisplayBootBitmap(
     _In_ BOOLEAN TextMode)
 {
+    static BOOLEAN FirstCall = TRUE;
+
 DPRINT1("DisplayBootBitmap\n");
 
-#ifdef BOOT_ROTBAR_IMPLEMENTED
-    /* Check if the animation has already been created */
+    /* If the animation has already been created,
+     * pause the animation but keep it alive */
     if (AnimationActive)
     {
-        /* Yes, just reset the progress bar but keep the animation alive */
         InbvAcquireLock();
-        RotBarSelection = RB_UNSPECIFIED;
+        /// PltRotBarStatus = 0;
+#if 1
+        /* Stop the animation -- In DPC mode the animation
+         * can be killed in the string display filter */
+        PltRotBarStatus = RBS_STOP_ANIMATE;
+        AnimationActive = FALSE;
+#endif
         InbvReleaseLock();
     }
-#endif
 
     ShowProgressBar = FALSE;
 
@@ -305,27 +300,26 @@ DPRINT1("DisplayBootBitmap\n");
     {
         PVOID Header, Footer;
 
+        InbvSolidColorFill(0, 0, SCREEN_WIDTH-1, SCREEN_HEIGHT-1, BV_COLOR_BLACK);
+        InbvSetTextColor(BV_COLOR_WHITE);
+
         /*
          * Make the kernel resource section temporarily writable,
          * as we are going to change the bitmaps' palette in place.
          */
         InbvMakeKernelResourceSectionWritable();
 
-        /* Check the type of the OS: workstation or server */
-        InbvSetTextColor(BV_COLOR_WHITE);
-        InbvSolidColorFill(0, 0, SCREEN_WIDTH-1, SCREEN_HEIGHT-1, BV_COLOR_BLACK);
-        InbvSolidColorFill(0, VID_FOOTER_BG_TOP, SCREEN_WIDTH-1, SCREEN_HEIGHT-1, BV_COLOR_LIGHT_GRAY);
-
 #if 0
+        /* Check the type of the OS: workstation or server */
         if (SharedUserData->NtProductType == NtProductWinNt)
         {
-            /* Workstation - Get resources */
+            /* Workstation; get resources */
             Header = InbvGetResourceAddress(IDB_WKSTA_HEADER);
             Footer = InbvGetResourceAddress(IDB_WKSTA_FOOTER);
         }
         else
         {
-            /* Server - Get resources */
+            /* Server; get resources */
             Header = InbvGetResourceAddress(IDB_SERVER_HEADER);
             Footer = InbvGetResourceAddress(IDB_SERVER_FOOTER);
         }
@@ -334,19 +328,25 @@ DPRINT1("DisplayBootBitmap\n");
         Footer = InbvGetResourceAddress(IDB_WKSTA_FOOTER);
 #endif
 
-        /* Set the scrolling region */
-        InbvSetScrollRegion(VID_SCROLL_AREA_LEFT, VID_SCROLL_AREA_TOP,
-                            VID_SCROLL_AREA_RIGHT, VID_SCROLL_AREA_BOTTOM);
-
-        /* Make sure we have resources */
-        if (Header && Footer)
+        /* If we have the resources, BitBlt them on the screen,
+         * first the footer and then the header, such that it is
+         * the header bitmap that dictates which palette to use. */
+        if (FirstCall && Footer)
         {
-            /* BitBlt them on the screen */
             BitBltAligned(Footer,
-                          TRUE,
+                          FALSE,
                           AL_HORIZONTAL_CENTER,
                           AL_VERTICAL_BOTTOM,
                           0, 0, 0, 0 /*65*/);
+        }
+        else
+        {
+            InbvSolidColorFill(0, VID_FOOTER_BG_TOP,
+                               SCREEN_WIDTH-1, SCREEN_HEIGHT-1,
+                               BV_COLOR_LIGHT_GRAY);
+        }
+        if (Header)
+        {
             BitBltAligned(Header,
                           FALSE,
                           AL_HORIZONTAL_CENTER,
@@ -354,33 +354,33 @@ DPRINT1("DisplayBootBitmap\n");
                           0, 0, 0, 0);
         }
 
-        // TODO: Improve: Don't show the progress bar if we do the transition
-        // full-screen to text mode and we are at the end of boot.
-        /* Set progress bar coordinates and display it */
-        InbvSetProgressBarCoordinates(VID_PROGRESS_BAR_LEFT,
-                                      VID_PROGRESS_BAR_TOP);
-
-#ifdef BOOT_ROTBAR_IMPLEMENTED
-        /* Store the line region to rotate in global buffer */
-        InbvScreenToBufferBlt(RotLineBuffer, 0,
-                              79-6 /* for header v2 */, // 122-6 /*for header v1*/, // SCREEN_HEIGHT-6,
-                              SCREEN_WIDTH, 6, SCREEN_WIDTH);
-
-        // {
-            // /* Hide the simple progress bar if not used */
-            // ShowProgressBar = FALSE;
-        // }
-#endif
-
         /* Restore the kernel resource section protection to be read-only */
         InbvMakeKernelResourceSectionReadOnly();
+
+        /* Set the scrolling region */
+        InbvSetScrollRegion(VID_SCROLL_AREA_LEFT, VID_SCROLL_AREA_TOP,
+                            VID_SCROLL_AREA_RIGHT, VID_SCROLL_AREA_BOTTOM);
+
+        /* Set the progress bar coordinates and display it,
+         * only if we directly are in text mode. If we do
+         * the transition full-screen to text mode and we are
+         * at the end of boot, don't show the progress bar. */
+        if (FirstCall)
+        {
+            InbvSetProgressBarCoordinates(VID_PROGRESS_BAR_LEFT,
+                                          VID_PROGRESS_BAR_TOP);
+        }
+
+        /* Set the rotating bar coordinates */
+        RotBarTop = VID_ROTBAR_TOP-6;
     }
     else
     {
         PVOID Screen;
 
         /* Is the boot driver installed? */
-        if (!InbvBootDriverInstalled) return;
+        if (!InbvBootDriverInstalled)
+            return;
 
         /*
          * Make the kernel resource section temporarily writable,
@@ -390,83 +390,98 @@ DPRINT1("DisplayBootBitmap\n");
 
         /* Load the boot screen */
         Screen = InbvGetResourceAddress(IDB_BOOT_SCREEN);
-
-        /* Make sure we have a logo */
         if (Screen)
         {
             /* Save the main image palette for implementing the fade-in effect */
             BootLogoCachePalette(Screen);
 
             /* Draw the screen */
-DPRINT1("Starting DRAWING SCREEN\n");
-            BitBltPalette(Screen, /*TRUE*/ FALSE, 0, 0);
-DPRINT1("DRAWING SCREEN finished\n");
+            BitBltPalette(Screen, FALSE /*TRUE*/, 0, 0);
 
-            /* Set progress bar coordinates and display it */
+            /* Set the progress bar coordinates and display it */
             InbvSetProgressBarCoordinates(VID_PROGRESS_BAR_LEFT,
                                           VID_PROGRESS_BAR_TOP);
         }
 
-#ifdef BOOT_ROTBAR_IMPLEMENTED
-        /* Store the line region to rotate in global buffer */
-        InbvScreenToBufferBlt(RotLineBuffer, 0, SCREEN_HEIGHT-6, SCREEN_WIDTH, 6, SCREEN_WIDTH);
-
-        // {
-            // /* Hide the simple progress bar if not used */
-            // ShowProgressBar = FALSE;
-        // }
-#endif
-
         /* Restore the kernel resource section protection to be read-only */
         InbvMakeKernelResourceSectionReadOnly();
 
-        /* Display the boot logo and fade it in */
-DPRINT1("Starting FADE-IN\n");
-        BootLogoFadeIn();
-DPRINT1("FADE-IN finished\n");
+        /* Set the rotating bar coordinates */
+        RotBarTop = SCREEN_HEIGHT-6;
+
+        /// /* Display the boot logo */
+        /// BootLogoFadeIn();
 
         /* Set filter which will draw text display if needed */
         InbvInstallDefaultDisplayStringFilter();
     }
 
-#ifdef BOOT_ROTBAR_IMPLEMENTED
+    /* Store the line region to rotate in the global buffer */
+    InbvScreenToBufferBlt(RotLineBuffer, 0, RotBarTop,
+                          SCREEN_WIDTH, 6, SCREEN_WIDTH);
+
+    /* Start the rotating bar animation if needed */
     if (!AnimationActive)
         AnimationActive = InbvAnimationInit(/*BootAnimUpdate,*/ 50);
-
-    /* Do we have the animation running? */
     if (AnimationActive)
     {
-        /* We do, initialize the progress bar */
+        /* The animation is running, initialize the progress bar */
         InbvAcquireLock();
-        RotBarSelection = (TextMode ? RB_TEXT : RB_SCREEN);
-        InbvRotBarInit();
+        PltRotBarStatus = RBS_FADEIN;
         InbvReleaseLock();
     }
-#endif
+
+    FirstCall = FALSE;
 }
 
 // CODE_SEG("INIT")
 VOID
 BootThemeCleanup(VOID)
 {
-    /* Check the display state */
-    // if (InbvGetDisplayState() == INBV_DISPLAY_STATE_OWNED)
-    // {
-        // /* Clear the screen */
-        // VidSolidColorFill(0, 0, SCREEN_WIDTH-1, SCREEN_HEIGHT-1, BV_COLOR_BLACK);
-    // }
-
-DPRINT1("FinalizeBootLogo\n");
-
-    /* Reset progress bar */
-#ifdef BOOT_ROTBAR_IMPLEMENTED
+#if 0
+    /* Stop the animation */
     PltRotBarStatus = RBS_STOP_ANIMATE;
     AnimationActive = FALSE;
 #endif
 }
 
-BOOLEAN
+
+/**
+ * @brief
+ * Helper used to show the shutdown message for both
+ * regular shutdown and shutdown after hibernation.
+ **/
+static BOOLEAN
 BootThemeDisplayShutdownMessage(
+    _In_ BOOLEAN TextMode,
+    _In_opt_ PCSTR Message)
+{
+    if (TextMode)
+    {
+        /* Center the message on screen. This code expects the message
+         * to be 34 characters long, on a 80x25-character screen. */
+        InbvDisplayString("                       ");
+        return TRUE; // Caller should display the message
+    }
+    else
+    {
+        /* Display the shutdown message */
+        PUCHAR Logo = InbvGetResourceAddress(IDB_SHUTDOWN_MSG);
+        if (Logo)
+        {
+            // InbvBitBlt(Logo, VID_SHUTDOWN_LOGO_LEFT, VID_SHUTDOWN_LOGO_TOP);
+            BitBltAligned(Logo,
+                          FALSE,
+                          AL_HORIZONTAL_CENTER,
+                          AL_VERTICAL_CENTER,
+                          0, 0, 0, 0);
+        }
+    }
+    return FALSE;
+}
+
+BOOLEAN
+BootThemeDisplaySafeToPowerOffScreen(
     _In_ BOOLEAN TextMode,
     _In_opt_ PCSTR Message)
 {
@@ -476,25 +491,91 @@ BootThemeDisplayShutdownMessage(
          * to be 34 characters long, on a 80x25-character screen. */
         ULONG i;
         for (i = 0; i < 25; ++i) InbvDisplayString("\r\n");
-        InbvDisplayString("                       ");
-        return TRUE; // Caller should display the message
+        // InbvDisplayString("                       ");
     }
     else
     {
-        PUCHAR Logo1, Logo2;
+        /* Acquire ownership if needed, and reset the display */
+        if (!InbvCheckDisplayOwnership()) // (InbvGetDisplayState() == INBV_DISPLAY_STATE_LOST)
+            InbvAcquireDisplayOwnership();
+        InbvResetDisplay();
 
-        /* Yes we do, cleanup for shutdown screen */
+        /* No string filter needed, we display everything */
+        InbvInstallDisplayStringFilter(NULL);
+        InbvEnableDisplayString(TRUE);
+
+        /* Cleanup for shutdown screen */
+        InbvSolidColorFill(0, 0, SCREEN_WIDTH - 1, SCREEN_HEIGHT - 1, BV_COLOR_BLACK);
+        InbvSetScrollRegion(0, 0, SCREEN_WIDTH - 1, SCREEN_HEIGHT - 1);
+    }
+    return BootThemeDisplayShutdownMessage(TextMode, Message);
+}
+
+BOOLEAN
+BootThemeHiberProgressInit(
+    _In_ BOOLEAN TextMode,
+    _In_opt_ PCSTR Message)
+{
+    if (TextMode)
+    {
+        // InbvResetDisplay();
+
+        /* Center the message on screen. This code expects the message
+         * to be 34 characters long, on a 80x25-character screen. */
+        ULONG i;
+        for (i = 0; i < 25; ++i) InbvDisplayString("\r\n");
+        InbvDisplayString("                       ");
+
+        /* Always show progress */
+        ShowProgressBar = TRUE;
+
+        return TRUE; // Caller should display the message
+    }
+    else // if (InbvBootDriverInstalled)
+    {
+        PUCHAR Logo;
+
+        /* Acquire ownership and reset the display */
+        InbvAcquireDisplayOwnership();
+        InbvResetDisplay();
+
+        /* No string filter needed, we display everything */
+        InbvInstallDisplayStringFilter(NULL);
+        InbvEnableDisplayString(TRUE);
+
+        /* Cleanup for hibernation screen */
         InbvSolidColorFill(0, 0, SCREEN_WIDTH - 1, SCREEN_HEIGHT - 1, BV_COLOR_BLACK);
         InbvSetScrollRegion(0, 0, SCREEN_WIDTH - 1, SCREEN_HEIGHT - 1);
 
-        /* Display shutdown logo and message */
-        Logo1 = InbvGetResourceAddress(IDB_SHUTDOWN_MSG);
-        Logo2 = InbvGetResourceAddress(IDB_LOGO_DEFAULT);
-        if (Logo1 && Logo2)
+        /* Display hibernation logo and message */
+        Logo = InbvGetResourceAddress(IDB_HIBERNATE_BAR);
+        if (Logo)
         {
-            InbvBitBlt(Logo1, VID_SHUTDOWN_MSG_LEFT, VID_SHUTDOWN_MSG_TOP);
-            InbvBitBlt(Logo2, VID_SHUTDOWN_LOGO_LEFT, VID_SHUTDOWN_LOGO_TOP);
+            // InbvBitBlt(Logo, VID_HIBER_MSG_LEFT, VID_HIBER_MSG_TOP);
+            BitBltAligned(Logo,
+                          FALSE,
+                          AL_HORIZONTAL_CENTER,
+                          AL_VERTICAL_CENTER,
+                          0, 0, 0, 0);
         }
+
+        /* Set the progress bar range and coordinates, and display it */
+        InbvSetProgressBarSubset(0, 100);
+        InbvSetProgressBarCoordinates(VID_HIBER_PRGBAR_LEFT,
+                                      VID_HIBER_PRGBAR_TOP);
     }
     return FALSE;
+}
+
+BOOLEAN
+BootThemeHiberProgressDone(
+    _In_ BOOLEAN TextMode,
+    _In_opt_ PCSTR Message)
+{
+    if (TextMode)
+    {
+        /* Add some space after the hibernation progress */
+        InbvDisplayString("\r\n");
+    }
+    return BootThemeDisplayShutdownMessage(TextMode, Message);
 }
