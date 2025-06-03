78afd131f1f497d665e7bd12f09bbc4ccf3e43b5
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2025-06-03 17:52:52 +0200

    [CMAKE] Introduce remove_target_compile_definition
    A function to remove target-specific compile preprocessor definitions,
    previously added with target_compile_definitions(<target> PRIVATE ...)

--- sdk/cmake/compilerflags.cmake
+++ sdk/cmake/compilerflags.cmake
@@ -1,7 +1,16 @@
 
-# remove_target_compile_options
+# remove_target_compile_definition
+#  Remove one option from the target COMPILE_DEFINITIONS property,
+#  previously added with target_compile_definitions(<target> PRIVATE ...)
+function(remove_target_compile_definition _module _definition)
+    get_target_property(_definitions ${_module} COMPILE_DEFINITIONS)
+    list(REMOVE_ITEM _definitions ${_definition})
+    set_target_properties(${_module} PROPERTIES COMPILE_DEFINITIONS "${_definitions}")
+endfunction()
+
+# remove_target_compile_option
 #  Remove one option from the target COMPILE_OPTIONS property,
-#  previously added through add_compile_options
+#  previously added with target_compile_options(<target> PRIVATE ...)
 function(remove_target_compile_option _module _option)
     get_target_property(_options ${_module} COMPILE_OPTIONS)
     list(REMOVE_ITEM _options ${_option})
